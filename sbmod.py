16d818c3f36ad06e220cd74095656c298fb97929
Bryce Boe <bbzbryce@gmail.com>
2025-02-15 16:35:54 -0800

    Add tests and implement pass/fail functions

--- sbmod.py
+++ sbmod.py
@@ -1,20 +1,30 @@
 """Moderation tool to help moderate r/SantaBarbara."""
 
+import argparse
 import logging
 import sys
 import time
-from datetime import datetime
+from collections import Counter
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
-import praw
 import prawcore
+from praw import Reddit
+from praw.models import Comment, Redditor, Subreddit
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 BOT = "sbmodbot"
 SUBREDDIT = "santabarbara"
+SUBREDDITS_TO_SHOW = 10
 TIMEZONE = ZoneInfo("America/Los_Angeles")
+
+DATES = {
+    "created": datetime.now(tz=TIMEZONE) - timedelta(days=14),
+    "history": datetime(year=2024, month=11, day=5, tzinfo=TIMEZONE),
+    "positive_karma": datetime(year=2025, month=1, day=20, tzinfo=TIMEZONE),
+}
 
 
 def _d(timestamp: float, /) -> datetime:
@@ -24,65 +34,127 @@ def _d(timestamp: float, /) -> datetime:
 class Verification:
     """Analyze and provide report on a redditor's activity history."""
 
-    def __init__(self, redditor: praw.models.Redditor, /) -> None:
+    def __init__(self, *, redditor: Redditor, subreddit: Subreddit) -> None:
         """Store information about this particular Verification."""
         self._redditor = redditor
-        self.comments: list[praw.models.Comment] = []
-        self.error: str
+        self._subreddit = subreddit
+        self.comments: list[Comment] = []
+        self.error: str | None = None
         self.found_comments = 0
-        self.subreddits: set[praw.models.Subreddits] = set()
+        self.karma: int | None = None
+        self.karma_average: float | None = None
+        self.note_types = Counter()
+        self.subreddits: Counter[Subreddit] = Counter()
 
     @property
     def created(self) -> datetime:
         """Return the datetime the ``Redditor`` was created."""
         return _d(self._redditor.created_utc)
 
-    def _process_comments(self) -> None:
+    def _process(self) -> bool:
+        """Validate the redditor."""
+        self._redditor_status()
+        if self.error is not None:
+            return False
+
+        if not self._process_notes():
+            return False
+        return self._process_comments()
+
+    def _process_comments(self) -> bool:
         """Fetch as many comments for the redditor and save some information."""
         log.info("fetching comments for %s", self._redditor)
-        for comment in self._redditor.comments.new(limit=None):
+        for comment in self._redditor.comments.new(limit=1000):
             self.found_comments += 1
-            if comment.subreddit != SUBREDDIT:
-                self.subreddits.add(comment.subreddit)
+            self.subreddits[comment.subreddit] += 1
+            if comment.subreddit != self._subreddit:
                 continue
             self.comments.append(comment)
         self.comments.sort(key=lambda x: x.created_utc)
 
-    def process(self) -> bool:
-        """Validate the redditor."""
-        try:
-            if self._redditor.is_suspended:
-                self.error = "is suspended"
-                return False
-        except prawcore.exceptions.NotFound:
-            self.error = "is not found"
+        if not self.comments:
+            self.error = f"has no r/{self._subreddit} history."
             return False
-        self._process_comments()
+
+        oldest_comment_date = _d(self.comments[0].created_utc)
+        if oldest_comment_date > DATES["positive_karma"]:
+            self.error = f"oldest r/{self._subreddit} comment is too recent ({oldest_comment_date})"
+            return False
+
+        self.karma = sum(comment.score for comment in self.comments)
+        self.karma_average = self.karma / len(self.comments)
+
+        if oldest_comment_date > DATES["history"] and self.karma_average < 1:
+            self.error = "too low of karma average"
+            return False
         return True
+
+    def _process_notes(self) -> bool:
+        """Collect counts of mod notes. Return true if validation should continue."""
+        for note in self._subreddit.mod.notes.redditors(self._redditor, limit=None):
+            self.note_types[note.type] += 1
+        if (bans := self.note_types["BAN"]) > 0:
+            self.error = f"has {bans} ban(s). Skipped history collection."
+            return False
+        if (mutes := self.note_types["MUTE"]) > 0:
+            self.error = f"has {mutes} mute(s). Skipped history collection."
+            return False
+        return True
+
+    def _redditor_status(self) -> None:
+        try:
+            self._redditor.is_blocked  # noqa: B018
+        except prawcore.exceptions.NotFound:
+            self.error = "is not found. No history information available."
+            return
+
+        if getattr(self._redditor, "is_suspended", False):
+            self.error = "is suspended. No history information available."
+            return
+
+        if self.created > DATES["created"]:
+            self.error = f"was created too recently ({self.created}). Skipped history collection."
+            return
+
+    def report(self) -> str:
+        """Return a report that is reddit markdown formatted."""
+        if self._process():
+            return self.results()
+        return f"u/{self._redditor.name}: verification fail\n\nAccount {self.error}"
 
     def results(self) -> str:
         """Return a reddit markdown report for the verification."""
-        karma = sum(comment.score for comment in self.comments)
-        average_karma = karma / len(self.comments)
         lines = []
-        lines.append(f"                User: {self._redditor}")
+        lines.append(f"                User: {self._redditor.name}")
         lines.append(f"             Created: {self.created}")
         lines.append(f"Commented subreddits: {len(self.subreddits)}")
+
+        if len(self.subreddits) > SUBREDDITS_TO_SHOW:
+            top_subreddits = self.subreddits.most_common(SUBREDDITS_TO_SHOW)
+            lines.append(f"   Top {SUBREDDITS_TO_SHOW} subreddits:")
+        else:
+            top_subreddits = self.subreddits.most_common(None)
+        for subreddit, count in top_subreddits:
+            lines.append(f"                      - {subreddit} ({count} comments)")
+
         lines.append(f"Total comments found: {self.found_comments}")
         lines.append("")
-        lines.append(f"{SUBREDDIT} specific")
+        lines.append(f"r/{self._subreddit} specific")
         lines.append(f"            Comments: {len(self.comments)}")
-        lines.append(f"       Comment karma: {karma}")
-        lines.append(f"       Average karma: {average_karma:.02f}")
-        lines.append(f"      Newest comment: {_d(self.comments[-1].created_utc)}")
-        lines.append(f"      Oldest comment: {_d(self.comments[0].created_utc)}")
-        return "\n".join(f"    {line}" for line in lines)
+
+        if self.comments:
+            lines.append(f"       Comment karma: {self.karma}")
+            lines.append(f"       Average karma: {self.karma_average:.02f}")
+            lines.append(f"      Newest comment: {_d(self.comments[-1].created_utc)}")
+            lines.append(f"      Oldest comment: {_d(self.comments[0].created_utc)}")
+
+        for note_type, count in sorted(self.note_types.items()):
+            lines.append(f"{note_type:>14} count: {count}")
+        return "\n".join(f"    {line}" if line else "" for line in lines)
 
 
-def handle_modmail(*, reddit: praw.Reddit) -> None:
+def handle_modmail(*, reddit: Reddit, subreddit: Subreddit) -> None:
     """Loop through all mod-specific conversations for actions."""
-    subreddit = reddit.subreddit(SUBREDDIT)
-
     for conversation in subreddit.modmail.conversations(state="mod", limit=None):
         if conversation.subject != "verify" or BOT in conversation.authors:
             continue
@@ -93,23 +165,31 @@ def handle_modmail(*, reddit: praw.Reddit) -> None:
         message = conversation.messages[0]
         username = message.body_markdown
         redditor = reddit.redditor(username)
-        verification = Verification(redditor)
-        if verification.process():
-            conversation.reply(body=verification.results())
-        else:
-            conversation.reply(body=f"u/{redditor.name} {verification.error}. No history information available.")
+        verification = Verification(redditor=redditor, subreddit=subreddit)
+        report = verification.report()
+        conversation.reply(body=report)
 
 
 def main() -> int:
     """Entrypoint to the program."""
-    reddit = praw.Reddit("sbmod", user_agent="SBModTool by u/bboe v0.0.1")
+    reddit = Reddit("sbmod", user_agent="SBModTool by u/bboe v0.0.1")
+    subreddit = reddit.subreddit(SUBREDDIT)
 
-    while True:
-        try:
-            handle_modmail(reddit=reddit)
-        except Exception:
-            log.exception("handle modmail failed")
-        time.sleep(60)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--report", metavar="redditor", help="Run report against a single user")
+    parser.add_argument("--watch", action="store_true", help="Monitor modmail for requests")
+    arguments = parser.parse_args()
+
+    if arguments.report:
+        verification = Verification(redditor=reddit.redditor(arguments.report), subreddit=subreddit)
+        print(verification.report())
+    elif arguments.watch:
+        while True:
+            try:
+                handle_modmail(reddit=reddit, subreddit=subreddit)
+            except Exception:
+                log.exception("handle modmail failed")
+            time.sleep(60)
     return 0
 
 
---
+++ tests/test_sbmod.py
@@ -0,0 +1,241 @@
+from unittest.mock import MagicMock, Mock, PropertyMock
+
+from prawcore.exceptions import NotFound
+
+from sbmod import DATES, SUBREDDIT, Verification, _d
+
+
+def create_mock_comment(*, created: float = 1700000000, score: int = 1, subreddit: Mock) -> Mock:
+    """Return an object like praw.models.Comment."""
+    comment = Mock()
+    comment.created_utc = created
+    comment.score = score
+    comment.subreddit = subreddit
+    return comment
+
+
+def create_mock_note(*, type_: str = "BAN") -> Mock:
+    """Return an object like praw.models.ModNote."""
+    note = Mock()
+    note.type = type_
+    return note
+
+
+def create_mock_redditor(
+    *,
+    comments: list[Mock] = None,
+    created: float = DATES["created"].timestamp(),
+    is_not_found: bool = False,
+    is_suspended: bool = False,
+    name: str = "redditor",
+) -> Mock:
+    """Return an object like praw.models.Redditor."""
+    redditor = Mock()
+    redditor.created_utc = created
+    if is_not_found:
+        type(redditor).is_blocked = PropertyMock(side_effect=NotFound(Mock()))
+    redditor.is_suspended = is_suspended
+    redditor.name = name
+    redditor.comments.new = Mock(return_value=[] if comments is None else comments)
+    return redditor
+
+
+def create_mock_subreddit(*, name: str = SUBREDDIT, notes: list[Mock] = None) -> Mock:
+    """Return an object like praw.models.Subreddit."""
+    subreddit = MagicMock()
+    subreddit.__str__.return_value = name  # pyright: ignore[reportFunctionMemberAccess]
+    subreddit.mod.notes.redditors = Mock(return_value=[] if notes is None else notes)
+    return subreddit
+
+
+def test_verification__is_not_found() -> None:
+    mock_redditor = create_mock_redditor(is_not_found=True, name="notfound")
+    mock_subreddit = create_mock_subreddit()
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert (
+        verification.report()
+        == "u/notfound: verification fail\n\nAccount is not found. No history information available."
+    )
+
+
+def test_verification__is_suspended() -> None:
+    mock_redditor = create_mock_redditor(is_suspended=True, name="suspended")
+    mock_subreddit = create_mock_subreddit()
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert (
+        verification.report()
+        == "u/suspended: verification fail\n\nAccount is suspended. No history information available."
+    )
+
+
+def test_verification__is_too_new() -> None:
+    mock_redditor = create_mock_redditor(created=DATES["created"].timestamp() + 0.001, name="toonew")
+    mock_subreddit = create_mock_subreddit()
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert (
+        verification.report()
+        == f"u/toonew: verification fail\n\nAccount was created too recently ({_d(mock_redditor.created_utc)}). Skipped history collection."
+    )
+
+
+def test_verification__has_ban() -> None:
+    mock_redditor = create_mock_redditor(name="hasban")
+    mock_subreddit = create_mock_subreddit(notes=[create_mock_note()])
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert verification.report() == "u/hasban: verification fail\n\nAccount has 1 ban(s). Skipped history collection."
+
+
+def test_verification__has_bans() -> None:
+    mock_redditor = create_mock_redditor(name="hasbans")
+    mock_subreddit = create_mock_subreddit(notes=[create_mock_note(), create_mock_note()])
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert verification.report() == "u/hasbans: verification fail\n\nAccount has 2 ban(s). Skipped history collection."
+
+
+def test_verification__has_mutes() -> None:
+    mock_redditor = create_mock_redditor(name="hasmutes")
+    mock_subreddit = create_mock_subreddit(notes=[create_mock_note(type_="MUTE"), create_mock_note(type_="MUTE")])
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert (
+        verification.report() == "u/hasmutes: verification fail\n\nAccount has 2 mute(s). Skipped history collection."
+    )
+
+
+def test_verification__has_no_history() -> None:
+    mock_redditor = create_mock_redditor(name="nohistory")
+    mock_subreddit = create_mock_subreddit()
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert verification.report() == "u/nohistory: verification fail\n\nAccount has no r/santabarbara history."
+
+
+def test_verification__insufficient_karma__lower_bound() -> None:
+    mock_subreddit = create_mock_subreddit()
+    mock_redditor = create_mock_redditor(
+        comments=[create_mock_comment(created=DATES["history"].timestamp() + 1, score=0, subreddit=mock_subreddit)]
+    )
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert verification.report() == "u/redditor: verification fail\n\nAccount too low of karma average"
+
+
+def test_verification__insufficient_karma__upper_bound() -> None:
+    mock_subreddit = create_mock_subreddit()
+    mock_redditor = create_mock_redditor(
+        comments=[create_mock_comment(created=DATES["positive_karma"].timestamp(), score=0, subreddit=mock_subreddit)]
+    )
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert verification.report() == "u/redditor: verification fail\n\nAccount too low of karma average"
+
+
+def test_verification__oldest_comment_too_recent() -> None:
+    mock_subreddit = create_mock_subreddit()
+    mock_redditor = create_mock_redditor(
+        comments=[create_mock_comment(created=DATES["positive_karma"].timestamp() + 1, subreddit=mock_subreddit)]
+    )
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert (
+        verification.report()
+        == "u/redditor: verification fail\n\nAccount oldest r/santabarbara comment is too recent (2025-01-20 00:00:01-08:00)"
+    )
+
+
+def test_verification__pass_with_low_karma() -> None:
+    mock_subreddit = create_mock_subreddit()
+    mock_redditor = create_mock_redditor(
+        comments=[
+            create_mock_comment(created=DATES["history"].timestamp(), score=0, subreddit=mock_subreddit),
+            create_mock_comment(subreddit="a"),
+        ]
+    )
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert (
+        verification.report()
+        == f"""                    User: redditor
+                 Created: {DATES["created"]}
+    Commented subreddits: 2
+                          - santabarbara (1 comments)
+                          - a (1 comments)
+    Total comments found: 2
+
+    r/santabarbara specific
+                Comments: 1
+           Comment karma: 0
+           Average karma: 0.00
+          Newest comment: 2024-11-05 00:00:00-08:00
+          Oldest comment: 2024-11-05 00:00:00-08:00"""
+    )
+
+
+def test_verification__pass_with_many_subreddits() -> None:
+    mock_subreddit = create_mock_subreddit()
+    mock_redditor = create_mock_redditor(
+        comments=[
+            create_mock_comment(created=DATES["history"].timestamp(), score=0, subreddit=mock_subreddit),
+            create_mock_comment(subreddit="a"),
+            create_mock_comment(subreddit="b"),
+            create_mock_comment(subreddit="c"),
+            create_mock_comment(subreddit="d"),
+            create_mock_comment(subreddit="e"),
+            create_mock_comment(subreddit="f"),
+            create_mock_comment(subreddit="g"),
+            create_mock_comment(subreddit="h"),
+            create_mock_comment(subreddit="i"),
+            create_mock_comment(subreddit="j"),
+        ]
+    )
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert (
+        verification.report()
+        == f"""                    User: redditor
+                 Created: {DATES["created"]}
+    Commented subreddits: 11
+       Top 10 subreddits:
+                          - santabarbara (1 comments)
+                          - a (1 comments)
+                          - b (1 comments)
+                          - c (1 comments)
+                          - d (1 comments)
+                          - e (1 comments)
+                          - f (1 comments)
+                          - g (1 comments)
+                          - h (1 comments)
+                          - i (1 comments)
+    Total comments found: 11
+
+    r/santabarbara specific
+                Comments: 1
+           Comment karma: 0
+           Average karma: 0.00
+          Newest comment: 2024-11-05 00:00:00-08:00
+          Oldest comment: 2024-11-05 00:00:00-08:00"""
+    )
+
+
+def test_verification__pass_with_mod_notes() -> None:
+    mock_subreddit = create_mock_subreddit(
+        notes=[
+            create_mock_note(type_="APPROVAL"),
+            create_mock_note(type_="APPROVAL"),
+            create_mock_note(type_="REMOVAL"),
+        ]
+    )
+    mock_redditor = create_mock_redditor(
+        comments=[create_mock_comment(created=DATES["history"].timestamp(), score=0, subreddit=mock_subreddit)]
+    )
+    verification = Verification(redditor=mock_redditor, subreddit=mock_subreddit)
+    assert (
+        verification.report()
+        == f"""                    User: redditor
+                 Created: {DATES["created"]}
+    Commented subreddits: 1
+                          - santabarbara (1 comments)
+    Total comments found: 1
+
+    r/santabarbara specific
+                Comments: 1
+           Comment karma: 0
+           Average karma: 0.00
+          Newest comment: 2024-11-05 00:00:00-08:00
+          Oldest comment: 2024-11-05 00:00:00-08:00
+          APPROVAL count: 2
+           REMOVAL count: 1"""
+    )
