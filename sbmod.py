869c45460ef883aaa66442171df21424e71d5a4b
Bryce Boe <bbzbryce@gmail.com>
2025-02-15 22:50:11 -0800

    Add means to bulk verify users

--- sbmod.py
+++ sbmod.py
@@ -8,7 +8,7 @@ import time
 import traceback
 from collections import Counter
 from datetime import datetime, timedelta
-from typing import cast
+from typing import TextIO, cast
 from zoneinfo import ZoneInfo
 
 import prawcore
@@ -208,15 +208,40 @@ def handle_messages(*, reddit: Reddit, subreddit: Subreddit) -> None:
         item.mark_read()
 
 
+def list_active_redditors(subreddit: Subreddit) -> None:
+    """Output a list of the redditors who have commented in the most recent 1000 submissions."""
+    redditors = Counter()
+    log.info("fetching submissions")
+    submissions = list(subreddit.new(limit=None))
+    log.info("found %d submissions", len(submissions))
+    for submission in submissions:
+        submission.comments.replace_more(limit=0)
+        comments = submission.comments.list()
+        log.info("found %d comments", len(comments))
+        for comment in comments:
+            redditors[comment.author] += 1
+    print(redditors.most_common(None))
+
+
 def main() -> int:
     """Entrypoint to the program."""
     reddit = Reddit("sbmod", user_agent=USER_AGENT)
     subreddit = reddit.subreddit(SUBREDDIT)
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("--active", action="store_true", help="Obtain list of recently active users")
+    parser.add_argument("--from-list", action="store_true", help="Add contributors from stdin")
     parser.add_argument("--run", action="store_true", help="Monitor messages for requests")
     parser.add_argument("--verify", metavar="redditor", help="Verify a single user")
     arguments = parser.parse_args()
+
+    if arguments.active:
+        list_active_redditors(subreddit=subreddit)
+        return 0
+
+    if arguments.from_list:
+        process_redditors_from_list(fp=sys.stdin, reddit=reddit, subreddit=subreddit)
+        return 0
 
     if arguments.verify:
         result, report = process_redditor(redditor=reddit.redditor(arguments.verify), subreddit=subreddit)
@@ -241,6 +266,22 @@ def process_redditor(*, redditor: Redditor, subreddit: Subreddit) -> tuple[bool,
     else:
         subreddit.modmail(FAILED_VERIFICATION_CONVERSATION_ID).reply(body=report)
     return result, report
+
+
+def process_redditors_from_list(*, fp: TextIO, reddit: Reddit, subreddit: Subreddit) -> None:
+    """Add all redditors from list provided via fp."""
+    contributors = set(subreddit.contributor(limit=None))
+    log.info("Found %d contributors", len(contributors))
+    for line in fp.readlines():
+        username = line.strip()
+        if not username:
+            continue
+        redditor = reddit.redditor(username)
+        if redditor in contributors:
+            log.info("Already a contributor: %s", redditor)
+            continue
+
+        process_redditor(redditor=redditor, subreddit=subreddit)
 
 
 def run(*, reddit: Reddit, subreddit: Subreddit) -> None:
